f7f84375070e7b9444c2ad1b75df14e4450d07d2
Bhanu Prasad Merakanapalli <bhanuprasad.m@xcubelabs.com>
2015-10-16 13:23:09 +0530

    WebView-Dial-Intent

--- app/src/main/java/com/xcubelabs/bhanuprasadm/retrofitdemo/activity/MainActivity.java
+++ app/src/main/java/com/xcubelabs/bhanuprasadm/retrofitdemo/activity/MainActivity.java
@@ -1,12 +1,15 @@
 package com.xcubelabs.bhanuprasadm.retrofitdemo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -19,8 +22,9 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class MainActivity extends AppCompatActivity {
-    Button click;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton click;
+    Button btnShare;
     TextView tv;
     EditText edit_user;
     ProgressBar pbar;
@@ -31,15 +35,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        click = (Button) findViewById(R.id.btnSearch);
+        click = (ImageButton) findViewById(R.id.btnSearch);
         tv = (TextView) findViewById(R.id.tvData);
         edit_user = (EditText) findViewById(R.id.etSearch);
         pbar = (ProgressBar) findViewById(R.id.pg);
+        btnShare = (Button) findViewById(R.id.btnShare);
         pbar.setVisibility(View.INVISIBLE);
 
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnShare.setOnClickListener(this);
+        click.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.menu_to_webview){
+            startActivity(new Intent(MainActivity.this, WebViewActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSearch:
                 String user = edit_user.getText().toString();
                 pbar.setVisibility(View.VISIBLE);
 
@@ -65,25 +96,17 @@ public class MainActivity extends AppCompatActivity {
                         pbar.setVisibility(View.INVISIBLE);       //disable progressbar
                     }
                 });
-            }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
+                break;
+            case R.id.btnShare:
+                if(!TextUtils.isEmpty(tv.getText())){
+                    Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, tv.getText());
+                    sendIntent.setType("text/plain");
+                    startActivity(sendIntent);
+                }
+                break;
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
